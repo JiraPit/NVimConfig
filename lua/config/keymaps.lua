e6cba6888dd0b31e7028732367ee3612d9238c2b
Jira Pit <pitakwongjira@gmail.com>
2024-04-24 01:57:58 +0700

    04/24

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+LazyVim.safe_keymap_set({ "v", "n", "i" }, "<S-Up>", "5<Up>")
+LazyVim.safe_keymap_set({ "v", "n", "i" }, "<S-Down>", "5<Down>")
