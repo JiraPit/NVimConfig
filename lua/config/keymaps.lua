d0545f4cd458760358efb2ac4e1b41614f54ffba
Jira Pit <pitakwongjira@gmail.com>
2024-05-03 01:20:36 +0700

    05/03

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 LazyVim.safe_keymap_set({ "n" }, "<leader>i", "$a")
 LazyVim.safe_keymap_set({ "v", "n" }, "-", "$")
-LazyVim.safe_keymap_set({ "v", "n", "i" }, "<S-Down>", "5<Down>")
-LazyVim.safe_keymap_set({ "v", "n", "i" }, "<S-Up>", "5<Up>")
 LazyVim.safe_keymap_set({ "v", "n", "i" }, "<C-Left>", "b")
 LazyVim.safe_keymap_set({ "v", "n", "i" }, "<C-Right>", "w")
+LazyVim.safe_keymap_set({ "v", "n", "i" }, "<S-Down>", "5<Down>")
+LazyVim.safe_keymap_set({ "v", "n", "i" }, "<S-Up>", "5<Up>")
---
+++ lua/plugins/minimap.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.nvim",
+  version = false,
+  -- opts = function(_, opts)
+  --   require("mini.map").setup()
+  --   opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --     ["<S-m>"] = MiniMap.toggle(nil),
+  --     ["<CTRL-m>"] = MiniMap.open(nil),
+  --   })
+  -- end,
+}
