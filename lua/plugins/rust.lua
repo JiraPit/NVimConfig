81b59d6d71835aced2a1b1a694f0beeb42f7967e
Jira Pit <pitakwongjira@gmail.com>
2024-05-20 22:16:46 +0700

    Change from control to shift for navigation

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -8,9 +8,9 @@ return {
         "Saecki/crates.nvim",
         event = { "BufRead Cargo.toml" },
         opts = {
-          src = {
-            cmp = { enabled = true },
-          },
+          -- src = {
+          --   cmp = { enabled = true },
+          -- },
         },
       },
     },
