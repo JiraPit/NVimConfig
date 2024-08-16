18f83d811395df267e6fc789412e3d73c8426201
Jira Pit <pitakwongjira@gmail.com>
2024-08-16 11:32:42 +0700

    aug16

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "plugins" }, -- Import your custom plugins
