0bae93b3f116cda896a3cd52b0263ea14462f37a
Jira Pit <pitakwongjira@gmail.com>
2025-05-29 20:18:54 +0700

    remove go

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.git" },
